cf24c9dfddc30e34fed0288a39a018a73b168480
Dexsless <dexsless@gmail.com>
2024-04-30 12:24:08 +0700

    haari ke 2

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,16 @@
 <?php
 
+use App\Models\Siswa;
+use App\Models\Sekolah;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+Route::get('/siswa', function () {
+    // return Siswa::all();
+    return view('siswa');
+});
+Route::get('/sekolah', function () {
+    return view('sekolah');
 });
