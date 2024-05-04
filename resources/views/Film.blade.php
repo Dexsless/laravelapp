9f091ff88100f3ee01af254c8f05163a44e66759
Dexsless <dexsless@gmail.com>
2024-05-04 14:14:41 +0700

    hari ke 4

--- resources/views/Film.blade.php
+++ resources/views/Film.blade.php
@@ -63,10 +63,14 @@
 @endforeach --}}
 @extends('layout/layout')
 @section('content')
+@php
+    $film = App\Models\Film::all();
+@endphp
             <div class="album py-5 bg-light">
             <div class="container">
 
                 <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+                    @foreach ($film as $item)
                     <div class="col">
                         <div class="card shadow-sm">
                             <svg class="bd-placeholder-img card-img-top" width="100%" height="225"
@@ -74,12 +78,11 @@
                                 preserveAspectRatio="xMidYMid slice" focusable="false">
                                 <title>Placeholder</title>
                                 <rect width="100%" height="100%" fill="#55595c" /><text x="50%" y="50%" fill="#eceeef"
-                                    dy=".3em">Thumbnail</text>
+                                    dy=".3em">{{$item->Judul}}</text>
                             </svg>
 
                             <div class="card-body">
-                                <p class="card-text">This is a wider card with supporting text below as a natural
-                                    lead-in to additional content. This content is a little bit longer.</p>
+                                <p class="card-text">{{$item->Deskripsi}}</p>
                                 <div class="d-flex justify-content-between align-items-center">
                                     <div class="btn-group">
                                         <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
@@ -90,6 +93,7 @@
                             </div>
                         </div>
                     </div>
+                    @endforeach
                 </div>
             </div>
         </div>
