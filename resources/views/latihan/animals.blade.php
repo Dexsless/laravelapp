19e3ef3505ea9830f01606c358ad5fee4953954f
Dexsless <dexsless@gmail.com>
2024-05-13 12:31:47 +0700

    hari ke 5

--- resources/views/latihan/animals.blade.php
+++ resources/views/latihan/animals.blade.php
@@ -2,16 +2,18 @@
 @section('content')
 <div class="container">
     <div class="row">
-        <div class="card">
-            <div class="card-header">
-                <h3>Daftar Hewan</h3>
-            </div>
-            <div class="card-body">
-                <ol>
-                    @foreach ($animal as $item)
-                    <li>{{ $item }}</li>
-                    @endforeach
-                </ol>
+        <div class="col-md-10">
+            <div class="card">
+                <div class="card-header">
+                    <h3>Daftar Hewan</h3>
+                </div>
+                <div class="card-body">
+                    <ol>
+                        @foreach ($animal as $item)
+                        <li>{{ $item }}</li>
+                        @endforeach
+                    </ol>
+                </div>
             </div>
         </div>
     </div>
